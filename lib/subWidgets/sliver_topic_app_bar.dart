64938743fbd74663b38dae325895e1b7506e8a31
Yuguang Ipsen <yuguang.ipsen@gmail.com>
2022-05-16 14:12:00 +1000

    background color for sliverAppBar

--- lib/subWidgets/sliver_topic_app_bar.dart
+++ lib/subWidgets/sliver_topic_app_bar.dart
@@ -6,21 +6,41 @@ class SliverTopicAppBar extends StatelessWidget {
   final String heading;
   @override
   Widget build(BuildContext context) {
-    return SliverAppBar(
-      elevation: 10.0,
-      expandedHeight: 120.0,
-      flexibleSpace: FlexibleSpaceBar(
-        titlePadding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
-        centerTitle: true,
-        title: Text(
-          '$heading',
-          softWrap: true,
-          style: kTextStyleCategory,
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Center(
+                    child: SliverAppBar(
+                      elevation: 10.0,
+                      expandedHeight: 120.0,
+                      backgroundColor: kWineRed,
+                      flexibleSpace: FlexibleSpaceBar(
+                        titlePadding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+                        centerTitle: true,
+                        title: Text(
+                          '$heading',
+                          softWrap: true,
+                          style: kTextStyleCategory,
+                        ),
+                      ),
+                      floating: true,
+                      pinned: false,
+                      // toolbarHeight: 100.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
-      floating: true,
-      pinned: false,
-      // toolbarHeight: 100.0,
     );
   }
 }
